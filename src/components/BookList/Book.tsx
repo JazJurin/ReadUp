75deefa77a3cd025c4c049da632bcb48f0a22542
JazJurin <jazjurin@gmail.com>
2024-01-20 16:51:10 +0100

    Book

--- src/components/BookList/Book.tsx
+++ src/components/BookList/Book.tsx
@@ -1,14 +1,20 @@
-
-export default function Book() {
+import { Link } from "react-router-dom";
+export default function Book(book) {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-  <figure className="px-10 pt-10">
-    <img src="" alt="Shoes" className="rounded-xl" />
-  </figure>
-  <div className="card-body items-center text-center">
-    <h2 className="card-title">Title</h2>
-    <p>Description</p>
+    <div className="flex flex-col sm:flex-row space-x-0 sm:space-x-4">
+    <div className="flex-none card w-full sm:w-96 bg-base-100 shadow-xl">
+      <figure className="px-10 pt-10">
+        <img src={book.cover_img} alt="Cover" className="rounded-xl" />
+      </figure>
+      <div className="card-body items-center text-center">
+        <Link to={`/book/${book.id}`} {...book}>
+          <h2 className="card-title">{book.title}</h2>
+          <p>Author: {book.author.join(", ")}</p>
+          <p>Edition: {book.edition_count}</p>
+          <p>First Publish Year: {book.first_publish_year}</p>
+        </Link>
+      </div>
+    </div>
   </div>
-</div>
-  )
+  );
 }
--- src/components/BookList/BookList.tsx
+++ src/components/BookList/BookList.tsx
@@ -2,36 +2,36 @@ import { useGlobalContext } from "../../context";
 import Loading from "../Loader/Loader";
 import cover from "../../assets/images/cover.jpg";
 import Book from "../BookList/Book";
+
 const BookList = () => {
-  const {books, loading, resultTitle} = useGlobalContext();
+  const { books, loading, resultTitle } = useGlobalContext();
+
   const booksWithCovers = books.map((singleBook) => {
     return {
       ...singleBook,
-      id: (singleBook.id).replace("/works/", ""),
-      cover_img: singleBook.cover_id ? `https://covers.openlibrary.org/b/id/${singleBook.cover_id}-L.jpg` : cover
-    }
+      id: singleBook.id.replace("/works/", ""),
+      cover_img: singleBook.cover_id
+        ? `https://covers.openlibrary.org/b/id/${singleBook.cover_id}-L.jpg`
+        : cover,
+    };
   });
 
-  console.log(booksWithCovers);
   if (loading) return <Loading />;
-  return (
-    <section className='booklist'>
-    <div className='container'>
-      <div className='section-title'>
-        <h2>{resultTitle}</h2>
-      </div>
-      <div className='booklist-content grid'>
-        {
-          booksWithCovers.slice(0, 30).map((item, index) => {
-            return (
-              <Book key = {index} {...item} />
-            )
-          })
-        }
-      </div>
-    </div>
-  </section>
-  )
-}
 
-export default BookList
+  return (
+    <section className="booklist">
+      <div className="container">
+        <div className="section-title">
+          <h2>{resultTitle}</h2>
+        </div>
+        <div className="booklist-content grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+          {booksWithCovers.slice(0, 30).map((item, index) => (
+            <Book key={index} {...item} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BookList;
